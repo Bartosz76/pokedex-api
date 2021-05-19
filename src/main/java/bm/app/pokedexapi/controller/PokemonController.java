825c6d43162316a9d50ebe797e31b90294952c73
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-05-19 16:13:50 +0200

    Created Pokemon controller that serves now requests for saving a Pokemon into a database.

--- src/main/java/bm/app/pokedexapi/controller/PokemonController.java
+++ src/main/java/bm/app/pokedexapi/controller/PokemonController.java
@@ -1,4 +1,15 @@
 package bm.app.pokedexapi.controller;
 
+import bm.app.pokedexapi.services.PokemonCreateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class PokemonController {
+
+    private final PokemonCreateService pokemonCreateService;
+
 }
