0119c1eba6937427111ce97bcfd32c65403067ae
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-05-19 10:12:15 +0200

    The initial commit.

---
+++ src/test/java/bm/app/pokedexapi/PokedexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bm.app.pokedexapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
